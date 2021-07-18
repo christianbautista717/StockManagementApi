644bb0b26cf049e248e0c90bc24cce9b51f48780
Social Codia <socialcodia@gmail.com>
2021-07-19 01:43:11 +0530

    Updated

--- SQL/stockmanagement.sql
+++ SQL/stockmanagement.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 19, 2021 at 10:14 PM
+-- Generation Time: Jul 18, 2021 at 10:11 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.6
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `stockmanagement`
+-- Database: `stockmanagement_empty`
 --
 
 -- --------------------------------------------------------
@@ -46,7 +46,7 @@ CREATE TABLE `admin` (
 
 INSERT INTO `admin` (`id`, `name`, `email`, `password`, `image`, `position`, `status`, `androidToken`, `webToken`, `timestamp`) VALUES
 (2, 'Social Codia', 'socialcodia@gmail.com', '$2y$10$mj40jM/kQRLg2Y5scscBouzna/e5I2RxyJSeRhbWjdAMm5vhg.h86', 'uploads/socialcodia.png', 1, 1, 'dH4Eet0hSkuKJockoWpyFd:APA91bFb4gfziC8L4cfFTajtDcl2fvzcM3ZYgZAdrW4iZ035QjKvFlHo0HNWJlucfWInnp20M4FW0FNzzBc_BiUBsPGGQW3bfaUtoFwmgDXfclCaDgGxgmjk0SFjzPgU9N8znp-K2xEW', 'fhRFxVSRZNUxRF7YtU8WpM:APA91bHni2PfGObvP0oJrLKz4gGfTlft-DzKcimUj_EyaS9A-jfAKNDXFux0KSRAQG63BQstKZvmlDo-NN7D_I1XpEV3VHZDME6DnSuuw0NhbramlFbmM0sQPFRdRczYqEA-nJvbWtNy', '2021-05-19 20:13:58'),
-(194, 'Umair Farooqui', 'info.umairfarooqui@gmail.com', '$2y$10$YaPKOJzmWfik7nh6VqteQ.JYBgTgCHjrMT7BdHMuYP1VFZdKoOIvG', 'uploads/mufazmi.png', 1, 1, 'csQO4wEpRh6P4H0rOOg860:APA91bGmqcjvtHu16ckD31DsNou9HCbcJgI2gyObrGaGqU1tiCVDnmen9dhgblE6FB5LpMmr8ExKItChBJp94FJrVGWHSfXP9psL1gqSD5gwv-WFitAEw8U8aL8EHFZTHEC7LoRfQxGy', 'fhRFxVSRZNUxRF7YtU8WpM:APA91bELidEfQDJoGdGsQtPDL9UaX1Zmumjf64kX1B_iAxie6x870XUKvw9Um51HjowlaCPWhb_q6c2Ie50WFLHnIeEy_C75TIj-HDEZAfMqngp-8_I81acTqPPTqQbydU77mgbQol1d', '2021-05-19 20:13:13');
+(194, 'Umair Farooqui', 'info.umairfarooqui@gmail.com', '$2y$10$YaPKOJzmWfik7nh6VqteQ.JYBgTgCHjrMT7BdHMuYP1VFZdKoOIvG', 'uploads/mufazmi.png', 1, 1, 'fuH0llQAS7aC5D3Nf7Q_op:APA91bFFXJNQBR7hBOOgkkUnDV3-9qS0AF5AdpgtJv6De2fROwN9RJvCxJ0jHRYgBj-NZMDD6rh5CWgr3TLjL7APptX59iMBtzEX84COfabgOMVonPc2uTymDdIJAZMWO3MpS83-DLth', 'fhRFxVSRZNUxRF7YtU8WpM:APA91bELidEfQDJoGdGsQtPDL9UaX1Zmumjf64kX1B_iAxie6x870XUKvw9Um51HjowlaCPWhb_q6c2Ie50WFLHnIeEy_C75TIj-HDEZAfMqngp-8_I81acTqPPTqQbydU77mgbQol1d', '2021-05-20 21:07:24');
 
 -- --------------------------------------------------------
 
@@ -297,6 +297,13 @@ CREATE TABLE `settings` (
   `productNoticeCount` int(55) NOT NULL DEFAULT 8
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `settings`
+--
+
+INSERT INTO `settings` (`settingId`, `productNoticeCount`) VALUES
+(1, 6);
+
 -- --------------------------------------------------------
 
 --
@@ -330,8 +337,8 @@ CREATE TABLE `updates` (
 --
 
 INSERT INTO `updates` (`updateId`, `updateTitle`, `updateDescription`, `updateVersion`, `updateUrl`, `updateTimestamp`) VALUES
-(1, 'Updated', 'Fixed Bugs', 1.3, 'https://apkfab.com/famblah/com.socialcodia.famblah/apk?h=d6ffa9e1bcb3a6d7f13e5cbb15ba7612c6210d819f71d91ed200adf7779b41d0', '2020-10-20 15:41:39'),
-(2, 'Updated Released 1.4', 'Fixed Bugs, And Improved User Interface', 3, 'https://apkfab.com/famblah/com.socialcodia.famblah/apk?h=d6ffa9e1bcb3a6d7f13e5cbb15ba7612c6210d819f71d91ed200adf7779b41d0', '2020-11-16 19:43:56');
+(1, 'Updated Released 1.3', 'Fixed Bugs', 1.3, 'https://www.youtube.com/channel/UCPCmuZrfxit5HyLFU6f6_nw/', '2021-07-18 20:09:45'),
+(2, 'Updated Released 1.4', 'Fixed Bugs, And Improved User Interface', 1.4, 'https://www.youtube.com/channel/UCPCmuZrfxit5HyLFU6f6_nw/', '2021-07-18 20:09:32');
 
 --
 -- Indexes for dumped tables
@@ -459,73 +466,73 @@ ALTER TABLE `updates`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=215;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=217;
 
 --
 -- AUTO_INCREMENT for table `brands`
 --
 ALTER TABLE `brands`
-  MODIFY `brand_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `brand_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `creditors`
 --
 ALTER TABLE `creditors`
-  MODIFY `creditorId` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `creditorId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `creditpayments`
 --
 ALTER TABLE `creditpayments`
-  MODIFY `paymentId` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `paymentId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `credits`
 --
 ALTER TABLE `credits`
-  MODIFY `creditId` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `creditId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `invoices`
 --
 ALTER TABLE `invoices`
-  MODIFY `invoice_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `invoice_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
-  MODIFY `itemId` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `itemId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `locations`
 --
 ALTER TABLE `locations`
-  MODIFY `location_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `location_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `payments`
 --
 ALTER TABLE `payments`
-  MODIFY `payment_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `payment_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `products_record`
 --
 ALTER TABLE `products_record`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `quantities`
@@ -537,31 +544,31 @@ ALTER TABLE `quantities`
 -- AUTO_INCREMENT for table `sellers`
 --
 ALTER TABLE `sellers`
-  MODIFY `sellerId` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `sellerId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `sellers_sells`
 --
 ALTER TABLE `sellers_sells`
-  MODIFY `sellers_sell_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `sellers_sell_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT for table `sells`
 --
 ALTER TABLE `sells`
-  MODIFY `sell_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `sell_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=134;
 
 --
 -- AUTO_INCREMENT for table `settings`
 --
 ALTER TABLE `settings`
-  MODIFY `settingId` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `settingId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `sizes`
 --
 ALTER TABLE `sizes`
-  MODIFY `size_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `size_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `updates`
